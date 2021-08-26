694aebef8b679e669c0ca9b5d78c532c322f7247
rebeccakuiper <46786848+rebeccakuiper@users.noreply.github.com>
2021-08-26 07:46:54 +0200

    Hands-on files (S18 Utrecht Summer School course)

---
+++ Hands-on files/Hands-on_2_GORIC_Compl_ANOVA_restriktor.R
@@ -0,0 +1,109 @@
+
+#### Evaluate H1 vs its complement with GORIC: ANOVA Example ####
+
+# Load libraries. These libraries contain functions such as 'goric' that will be used in this R code. 
+# Each time you reopen this R file you have to execute this step.
+#
+if (!require("psych")) install.packages("psych") # install this package first (once)
+library(psych) # for the function describeBy
+#
+# If you want to use restriktor from github:
+#if (!require("devtools")) install.packages("devtools")
+#library(devtools) 
+#install_github("LeonardV/restriktor")
+#library(restriktor) # for goric function
+# If from CRAN:
+if (!require("restriktor")) install.packages("restriktor") # install this package first (once)
+library(restriktor) # for goric function
+
+###################################################################################
+
+
+# Notably, it is only possible to load the data if you are using the correct working directory (with both your R script and data file). 
+# The command `getwd()` shows you your current working directory. 
+# You can change the working directory to the one you prefer using the function `setwd()` by specifying the correct location between parentheses. 
+# Alternatively, in Rstudio, you can use the "Session" tab (on top) or 
+# you can use the "Files"-pane (on top of probably the right lower box of your Rstudio-screen, this pane is located next to the panes for "Plots", "Packages", "Help" and "Viewer").
+
+
+### Example Lucas ### 
+
+# Read/Load the Data # 
+# If you open the data file `Data_Lucas.txt` in a text editor, 
+# you can see that the variable labels have been inserted (using quotes; i.e., "...") in the first line of the file, which is called a header. 
+# Therefore, you have to specify 'header = TRUE' when loading the data:
+Lucas <- read.table("Data_Lucas.txt", header=TRUE)
+
+# Make the variable group a factor #
+#Since we loaded a txt file, R does not know the measurment levels of the variables and assumes all to be continuous (so, interval or ratio). 
+#Hence, we need to tell R that the variable `group` is a factor (i.e., a grouping / categorical / nominal variable): 
+Lucas$group <- factor(Lucas$group)  # this command tells R that group is a factor and not a continuous variable like Influence
+
+# Inspect the data #
+head(Lucas) # Look at first (6) rows of the data
+
+# Compute descriptives for each group #
+descrstat <- describeBy(Lucas$Influence, Lucas$group, mat = TRUE)
+descrstat
+
+
+
+## Compute the GORIC ##
+
+# first need R object with unconstrained estimates (here, five group means and one residual variance)
+lm_fit_Lucas <-  lm(Influence ~ group-1, data = Lucas)
+# Note that:
+# 1. `y ~ group - 1` instructs the function `lm` (linear model) to regress y on group.
+# 2. The `- 1` instructs the function `lm` to drop the intercept and, therefore, 
+#    estimate the means in each group, resulting here in five group means. 
+#    On the other hand,  `y ~ group' would estimate an intercept, 
+#    representing the mean of the reference group, and 
+#    the mean differences between the other (here, four) groups and the reference group.
+# 3. The results are collected in, what is called, an R-object, named `lm_fit_lucas`.
+
+# Check the names used in model
+names(coef(lm_fit_Lucas))
+# Specify restrictions using those names
+
+# Hypotheses Set
+#
+# NOTES: 
+# - restriktor always needs pairs of restrictions!
+# - restriktor uses "==" to denote an equality restriction
+# - restriktor uses ";" to separate the restrictions within one hypothesis
+#
+H1 <- 'group5 == group3; group3 > group1; group3 > group4; group1 > group2; group4 > group2' # Note: H1 is not full row-rank, see the goric tutorial for more details.
+
+
+# Calculate GORIC values and weights
+#In the calculation of the GORIC, an iterative process is needed to calculate the penalty / complexity part. Therefore, one needs to set a seed value:
+#1. Then, you will obtain the same penalty value every time you run this code.
+#2. Then, you can change the seed value to check the sensitivity of the penalty value 
+#   If it is sensitive, then increase number of iterations used in calculation of the penalty (see below).
+set.seed(123) # Set seed value
+output_c <- goric(lm_fit_Lucas, H1, comparison = "complement")
+summary(output_c)
+# The order-restricted hypothesis H1 has  13.4 times more support than its complement.             
+
+
+###################################################################################
+
+
+restriktor: generalized order-restriced information criterion: 
+  
+  Results:
+  model    loglik  penalty    goric  goric.weights
+1          H1  -278.051    3.195  562.493          0.931
+2  complement  -278.048    5.795  567.688          0.069
+---
+  
+  Relative GORIC-weights:
+  vs. H1  vs. complement
+H1          1.000   13.431        
+complement  0.074   1.000         
+---
+  The order-restricted hypothesis ‘H1’ has 13.431 times more support than its complement.
+
+Restriktor message: Since the constraint matrix is not full row-rank, the level probabilities 
+are calculated using mix.weights = "boot" (the default is mix.weights = "pmvnorm").
+For more information see ?restriktor.
---
+++ Hands-on files/Hands-on_5_GORICA_CombEv_restriktor.R
@@ -0,0 +1,91 @@
+
+#### Updating/Aggregating evidence obtained with GORIC and GORICA: GORIC Evidence Synthesis Example
+
+
+# NOTE 1: This is work in progress. 
+# So, please only use the upcoming function yourself, and do not distribute this to others untill the package is freely available on GitHub (now, it is 'privately').
+# Also, let me know if you think things could be improved (e.g., way of asking for input, what should be reported as output, et cetera).
+
+# NOTE 2: Make sure you use the right version of restriktor, namely version 0.2-800 (or higher).
+# So, you should obtain the following:
+library(restriktor)
+#>This is restriktor 0.2-800
+#>restriktor is BETA software! Please report any bugs.
+# Version 0.2-500 might seem to work, but renders wrong results if the complement is used.
+
+
+
+#First, we need to load the required libraries (after installing them once). 
+#Since the `GoricEvSyn` package is only on GitHub (not on CRAN), we need `devtools` to install it. 
+#
+if (!require("devtools")) install.packages("devtools")
+library(devtools)
+#
+#install_github("rebeccakuiper/GoricEvSyn") #, auth_token = "6ae25b58364f3c0a2beeef95ba3fd0ad114566a8") # This way you get acces to this 'private' package
+library(GoricEvSyn)
+
+# If you want to see the help/description files of this function, you can use the following code:
+?GoricEvSyn
+?GoricEvSyn_IC
+?GoricEvSyn_LLandPT
+
+
+
+#### 4 trust studies described in Kuiper et al (2013): H0, Hpos, Hneg ####
+#In this example, we will use the data and hypotheses used in Kuiper et al. (2013). 
+#This article describes four diverse trust studies where each have a theory about the sign of the parameter of interest ($H_0$, $H_{pos}$ and $H_{neg}$). 
+#
+#Please note the following:
+#* You could also choose to discard the uninformative hypothesis $H_0$.
+#* The GORICA only requires the structural parameters! Hence, we do not need to use all the parameters, but only the one of interest.
+
+# Set the number of primary studies for which the evidence for a central theory should be determined and synthesized:
+S <- 4
+
+# In this example, all studies have the same number of parameters of interest (namely 1; i.e., the hypotheses below address only one parameter).
+# In that case, we can collect the study-specific estimates (i.e., the $\beta$-values from the studies) in one matrix: 
+# The estimates of the studies are placed in seperated rows, while the parameter estimates are placed in the columns. 
+# In this example, there are $S = 4$ rows and $nrParam = 1$ column. 
+# We also specify the name of this column accordingly to what it represents, namely `beta1`. Please make sure to name the column the same way as in the hypotheses below.
+nrParam <- 1
+param_studies <- matrix(c(0.09, 0.14, 1.09, 1.781), nrow = S, ncol = nrParam)
+colnames(param_studies) <- "beta1"
+param_studies # To check the resulting matrix
+
+#Furthermore, we need to specify the covariance matrix of the parameter estimates; for this matrix it is not necessary to specify the column names. 
+#Since there is only one parameter in the example, we only need the squared standard errors of the $\beta$-values. 
+covmx_studies <- matrix(c(0.029^2, 0.054^2, 0.093^2, 0.179^2), nrow = S, ncol = nrParam)
+
+
+## Evidence synthesis using the GORICA ##
+
+# Set hypotheses #
+
+# Now, we need to specify the hypotheses for all studies. 
+# In this example, all studies have the same set of hypotheses ('same_hypo <- 1'), consisting of three hypotheses ('nrHypos <- 3').
+same_hypo <- 1
+nrHypos <- 3
+#
+H0 <- "beta1 == 0"
+Hpos <- "beta1 > 0"
+Hneg <- "beta1 < 0"
+#
+hypo_studies <- c(H0, Hpos, Hneg)
+
+# Then, we also need to set a safeguard-hypothesis, to prevent choosing a best hypothesis from a set of weak hypotheses. 
+# In this example, the whole space of theories is covered by the three hypotheses. Therefore, we do not need a safeguard-hypothesis here.
+safeguard <- "none"
+
+
+# Evidence synthesis #
+# Before we can start with the evidence-synthesis, we need to set the type of evidence-synthesis. 
+# In this case, we will set it to 1 ('type_ev <- 1'), the added-evidence approach.
+type_ev <- 1
+GoricEvSyn(type_ev, S, param_studies, covmx_studies, same_hypo, nrHypos, hypo_studies, safeguard)
+#
+# From the last line in 'Cumulative.GORICA.weights', it can be seen that 'H2' (i.e., $H_{pos}$) is the most supported hypothesis and even has the maximum support.
+# From 'Final.rel.GORICA.weights', you can also see that is many more times supported than the other hypotheses.
+
+
+###################################################################################
+
