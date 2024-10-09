afcce15e32a996190ebe2ebcb8e9422fd4f980e3
Rebecca Kuiper <R.M.Kuiper@uu.nl>
2024-10-09 09:09:39 +0200

    Upload files KLI course

--- KLI course/1 _ Lab - R - NHST vs GORIC vs BMS/Possible answer GORIC incl Benchmarks.R
+++ KLI course/1 _ Lab - R - NHST vs GORIC vs BMS/Possible answer GORIC incl Benchmarks.R
@@ -1,5 +1,4 @@
-if (!require("psych")) install.packages("psych") # install this package first (once)
-library(psych) # for the function describeBy
+# Install and load packages
 
 if (!require("bain")) install.packages("bain") # install this package first (once)
 library(bain) # for bain function
@@ -7,8 +6,13 @@ library(bain) # for bain function
 if (!require("restriktor")) install.packages("restriktor") # install this package first (once)
 library(restriktor) # for goric function
 
+if (!require("psych")) install.packages("psych") # install this package first (once)
+library(psych) # for the function describeBy
+
 ###################################################################################
 
+
+# Exercise 1a&1b
 
 # Read/Load the Data # 
 # It comes from the bain package and is called sesamesim
@@ -37,7 +41,7 @@ names(coef(lm_fit_sesam))
 # Specify restrictions using those names.
 #Note: it often comes down to the name and levels of the factor(s).
 
-# Hypotheses Set
+# Hypothesis/-es
 #
 # As a reminder, the 5 groups are:
 #1.	 disadvantaged inner city,
@@ -45,17 +49,19 @@ names(coef(lm_fit_sesam))
 #3.	 advantaged rural,
 #4.	 disadvantaged rural,
 #5.	 disadvantage Spanish speaking.
-
-H12_sesam <- "site2>site3>site1>site4, site5>site4" # specify the hypothesis of interest
-# or: H12_sesam <- "site2>site3>site1>site4 & site5>site4"
+#
+# Theory-based hypothesis
+H1_sesam <- 'site2 > site3 > site4; 
+             site2 > (site1, site5)'
+# vs its complement (default in case of one hypothesis)
 
 
 # p-value null hypothesis test(s)
 summary(lm_fit_sesam)
 # Each mean is significantly different from 0.
 # And hypothesis that all means are zero is rejected as well 
-# (F-statistic: 46.08 on 5 and 235 DF,  p-value: < 2.2e-16).
-# But: We do not know anything yet about the orderings expected on forehand 
+# (F-statistic = 46.08 with 5 and 235 df's;  p-value = 2.2e-16 < .001).
+# But: We do not know anything yet about the orderings expected beforehand 
 #                      as specified in our theories/expectations/hypotheses.
 
 
@@ -67,12 +73,21 @@ summary(lm_fit_sesam)
 #   If it is sensitive, then increase number of iterations used in calculation of the penalty.
 set.seed(123) # Set seed value
 output <- goric(lm_fit_sesam, 
-                hypotheses = list(H12_sesam = H12_sesam), 
-                comparison = 'complement')
+                hypotheses = list(H1_sesam = H1_sesam))
 output
 #summary(output)
 #
-#The order-restricted hypothesis ‘H12_sesam’ has  6.5 times more support than its complement.
+#The order-restricted hypothesis ‘H1_sesam’ has  more support than its complement.
+#
+# So, on average, the data thus supports our hypothesis that 
+# children from a suburban advantaged background improve more than children from 
+# a rural one and that children from an advantaged background improve more than 
+# children from a disadvantaged background (for both rural and suburban).
+
+#If needed, you can use `Guidelines_output_GORIC.html’ 
+#from https://github.com/rebeccakuiper/Tutorials 
+#to interpret the GORIC output.
+
 
 #In case you want to inspect the order-restricted maximum likelihood estimates (or-mle), inspect:
 output$ormle
@@ -80,40 +95,33 @@ output$ormle
 # Note
 output$ormle
 output$b.unrestr 
-# complement is in agreement with data;
+# Our hypothesis is thus in agreement with data;
 # as we could also see from the maximum log likelihood values.
-
 
 
 #####
 
+# Exercise 3
+
 # Benchmarks GORIC
-
-# Install and load benchmark function
-if (!require("devtools")) install.packages("devtools")
-library(devtools)
-install_github("rebeccakuiper/benchmarks")
-library(benchmarks)
-# For more information regarding the included functions:
-#?benchmarks
-#?benchmarks_ANOVA
-
+#
+#More information can be found on the github repository 
+#https://github.com/rebeccakuiper/Tutorials:  
+#- 'Guidelines_GORIC-benchmarks' (html or pdf) and  
+#- 'Hands-on_123_Extra_Benchmarks_ANOVA_restriktor.R' (in the Hands-on files folder). 
+#
 #In the benchmark code below, 
-#the null population for which all means are equal is used. 
-#If you rather check for another null using say a small effect size, you can do that. 
-#You can either use a specific ANOVA effect size (pop.es) in the benchmarks_ANOVA function 
-#or specify a set of population estimations (pop.est) in the benchmarks function. 
-#When using the null where all means are equal:
-  
-# Example using the benchmarks_ANOVA function:
-pop.es <- c(0)
-benchmarks_1c <- benchmarks_ANOVA(output, pop.es)
-benchmarks_1c$error.prob.pref.hypo
-# There is a 13.4% change that H12_sesam is not the correct hypothesis.
-benchmarks_1c$benchmarks.weight
-benchmarks_1c$benchmarks.ratios
-# Our finding is quite extreme under the null (in between 65 and 95%).
-# Hence, there is high (compelling) support.
+#the null population for which ES = 0, that is, 
+#all means are equal is used. 
+# Use 'pop_es' to specify own null population(s).	
+#  
+# Benchmarks:
+benchmarks_sesam <- benchmark(goric_sesam, model_type = "means", ncpus = 8)
+benchmarks_sesam 
+plot(benchmarks_sesam, x_lim = c(0, 14))
+# Our finding is very extreme under the null:
+# Namely, our sample value is higher than the 95th percentile under the null. 
+# Hence, there is tremendous support for 'H1_sesam' (cf. 'Guidelines_GORIC-benchmarks').
 
 
 ###################################################################################
